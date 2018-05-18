c493efd4731e20a3718201721af520a8e8e6456e
HMBSbige <admin@bige0.com>
2018-05-18 23:38:45 +0800

    高仿程序完成

---
+++ NatTypeTester/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace NatTypeTester
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "STUN Server:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 47);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 12);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "NAT type:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 74);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(65, 12);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Local end:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 101);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(71, 12);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Public end:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(95, 17);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(174, 21);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "stun.ekiga.net";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(95, 44);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(240, 21);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(95, 71);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(240, 21);
+			this.textBox3.TabIndex = 6;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(95, 98);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(240, 21);
+			this.textBox4.TabIndex = 7;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(275, 17);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(60, 21);
+			this.numericUpDown1.TabIndex = 8;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            3478,
+            0,
+            0,
+            0});
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(260, 129);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Test";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(351, 164);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "NatTypeTester";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ NatTypeTester/MainForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LumiSoft.Net.STUN.Client;
+
+namespace NatTypeTester
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private delegate void VoidMethodDelegate();
+
+		private static string[] Core(string server, int port)
+		{
+			var socketv4 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			try
+			{
+				if (string.IsNullOrWhiteSpace(server))
+				{
+					MessageBox.Show(@"Please specify STUN server !", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return null;
+				}
+
+				socketv4.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+				var result = STUN_Client.Query(server, port, socketv4);
+
+				return new[]
+				{
+					result.NetType.ToString(),
+					socketv4.LocalEndPoint.ToString(),
+					result.NetType != STUN_NetType.UdpBlocked ? result.PublicEndPoint.ToString() : string.Empty
+				};
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($@"Error: {ex}", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return null;
+			}
+			finally
+			{
+				socketv4.Close();
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				button1.Enabled = false;
+				var t = new Task(() =>
+				{
+					BeginInvoke(new VoidMethodDelegate(() =>
+					{
+						var res = Core(textBox1.Text, Convert.ToInt32(numericUpDown1.Value));
+						if (res != null)
+						{
+							textBox2.Text = res[0];
+							textBox3.Text = res[1];
+							textBox4.Text = res[2];
+						}
+					}));
+				});
+				t.Start();
+				t.ContinueWith(task =>
+				{
+					BeginInvoke(new VoidMethodDelegate(() =>
+					{
+						button1.Enabled = true;
+					}));
+				});
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($@"Error: {ex}", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				button1.Enabled = true;
+			}
+		}
+	}
+}
